3eac7e7edb0031676c93c11c998e24b7eb5e2bf6
ExorCistWeb <75250883+ExorCistWeb@users.noreply.github.com>
2024-10-05 04:13:55 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".myTwoSwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+
+
+$(document).ready(function() {
+    $(".faq-question").click(function() {
+        $(this).toggleClass("active");
+        $(this).next(".faq-answer").slideToggle(300);
+        // Переключение иконки
+        var icon = $(this).find(".toggle-icon");
+
+    });
+});
