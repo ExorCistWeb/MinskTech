59ed6697f58d1d5d569ef7b55b57516f750ac468
ExorCistWeb <75250883+ExorCistWeb@users.noreply.github.com>
2024-10-06 20:19:12 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
+$(document).ready(function() {
+    // Открытие попапа авторизации
+    $('#open-login').click(function() {
+        $('#login-popup').fadeIn();
+    });
+
+    // Закрытие попапа авторизации
+    $('#close-login').click(function() {
+        $('#login-popup').fadeOut();
+    });
+
+    // Открытие попапа регистрации из авторизации
+    $('#open-register').click(function() {
+        $('#login-popup').fadeOut();
+        $('#register-popup').fadeIn();
+    });
+
+    // Закрытие попапа регистрации
+    $('#close-register').click(function() {
+        $('#register-popup').fadeOut();
+    });
+    // Открытие попапа авторизации из регистрации
+    $('#open-login-from-register').click(function() {
+        $('#register-popup').fadeOut();
+        $('#login-popup').fadeIn();
+    });
+
+    // Открытие попапа подтверждения кода из регистрации
+    $('#register-submit').click(function() {
+        $('#register-popup').fadeOut();
+        $('#confirm-popup').fadeIn();
+    });
+
+    // Закрытие попапа подтверждения кода
+    $('#close-confirm').click(function() {
+        $('#confirm-popup').fadeOut();
+    });
+});
+
 var swiper = new Swiper(".myTwoSwiper", {
     pagination: {
         el: ".swiper-pagination",
@@ -18,4 +57,16 @@ $(document).ready(function() {
         var icon = $(this).find(".toggle-icon");
 
     });
+});
+
+document.getElementById('filter-button').addEventListener('click', function() {
+    // Проверяем ширину экрана
+    if (window.innerWidth <= 768) {
+        const popupContent = document.getElementById('popup_content');
+        if (popupContent.style.display === 'block') {
+            popupContent.style.display = 'none'; // Закрыть попап
+        } else {
+            popupContent.style.display = 'block'; // Открыть попап
+        }
+    }
 });
